d2a0979f790d9409eaa529b826174cb15b114618
TanusreeIITM <119420447+TanusreeIITM@users.noreply.github.com>
2022-11-29 18:18:01 +0530

    Add files via upload

---
+++ web1.py
@@ -0,0 +1,26 @@
+import streamlit as st
+ 
+st.title("Calculator App using Streamlit")
+ 
+# creates a horizontal line
+st.write("---")
+ 
+# input 1
+num1 = st.number_input(label="Enter first number")
+ 
+# input 2
+num2 = st.number_input(label="Enter second number")
+ 
+
+ 
+
+ans = 0
+ 
+def calculate():
+    ans = num1 - num2
+
+ 
+    st.success(f"Answer = {ans}")
+ 
+if st.button("Calculate result"):
+    calculate()
